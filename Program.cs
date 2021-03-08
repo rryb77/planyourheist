d8933c0d6efc2ac650b054e059dc2cd507da6de5
Ryan Youngblood <rryb77@gmail.com>
2021-03-08 12:03:07 -0600

    Phase one complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace planyourheist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("");
+            Console.Write("Enter your team member name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your team members skill level: ");
+            int skill = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your team members courage factor (0.0 - 2.0): ");
+            double courage = Double.Parse(Console.ReadLine());
+
+            TeamMember numberOne = new TeamMember()
+            {
+                Name = name,
+                Skill = skill,
+                Courage = courage
+            };
+
+            Console.WriteLine($"{numberOne.Name} has a skill level of {numberOne.Skill} and a courage factor of {numberOne.Courage}");
+
+
+
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,12 @@
+namespace planyourheist
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public double Courage { get; set; }
+        public int Skill { get; set; }
+
+
+    }
+}
+
